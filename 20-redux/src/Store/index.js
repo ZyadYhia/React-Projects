275b27c851cc2fbb76fe30a61a9de2037f428ed8
ZyadYhiaJisr <z.yhia@jisr.net>
2025-02-03 14:32:39 +0200

    Finalize Redux

--- 20-redux/src/Store/index.js
+++ 20-redux/src/Store/index.js
@@ -1,14 +1,41 @@
 import { createStore } from 'redux'
 
-const storeReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true }
+
+const counterReducer = (state = initialState, action) => {
     if (action.type === 'INCREMENT') {
-        return state.counter++
+        // this way is a bad practice but will work
+        // return {
+        //     ...state,
+        //     counter: state.counter + 1,
+        // }
+        // instead we should return brand new object
+        return {
+            counter: state.counter + 1,
+            showCounter: state.showCounter
+        }
+    }
+    if (action.type === 'INCREASE') {
+        return {
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter
+        }
     }
     if (action.type === 'DECREMENT') {
-        return state.counter--
+        return {
+            counter: state.counter - 1,
+            showCounter: state.showCounter
+        }
+    }
+
+    if (action.type === 'TOGGLE') {
+        return {
+            counter: state.counter,
+            showCounter: !state.showCounter
+        }
     }
     return state
 }
 
-const store = createStore(storeReducer)
+const store = createStore(counterReducer)
 export default store

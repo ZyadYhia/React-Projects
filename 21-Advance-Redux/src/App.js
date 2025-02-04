beaec80bfd077ed675c6eaee7a23f08bb15bdc08
ZyadYhiaJisr <z.yhia@jisr.net>
2025-02-04 12:41:04 +0200

    use ActionCreator

--- 21-Advance-Redux/src/App.js
+++ 21-Advance-Redux/src/App.js
@@ -4,7 +4,7 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
 import { useEffect } from 'react';
-import { uiActions } from './Store/ui-slice';
+import { sendCartDate } from './Store/cart-slice';
 
 let isInit = true;
 
@@ -13,42 +13,13 @@ function App() {
   const showCart = useSelector(state => state.ui.isVisible);
   const cart = useSelector(state => state.cart);
   const notification = useSelector(state => state.ui.notification);
-  // useEffect will be executed once the application reloads
-  // this will cause that the cart will be saved in the database with initial values 
   useEffect(() => {
-    const sendCartData = async () => {
-
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Pending...',
-        message: 'Sending cart data!'
-      }))
-      const response = await fetch('https://reactdb-1e67f-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
-        method: 'PUT',
-        body: JSON.stringify(cart)
-      })
-      if (!response.ok) {
-        throw new Error('Sending cart data failed')
-      }
-      // const responseData = await response.json()
-
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Success',
-        message: 'Sending cart data succeeded!'
-      }))
-    }
     if (isInit) {
       isInit = false;
       return;
     }
-    sendCartData().catch(error => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error!',
-        message: 'Sending cart data failed!'
-      }))
-    })
+    dispatch(sendCartDate(cart));
+
   }, [cart, dispatch])
   return (
     <>
--- 21-Advance-Redux/src/Store/cart-slice.js
+++ 21-Advance-Redux/src/Store/cart-slice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
-
+import { uiActions } from './ui-slice'
 const initialState = {
     items: [],
     totalQuantity: 0,
@@ -34,12 +34,46 @@ const cartSlice = createSlice({
             if (existingItem.quantity === 1) {
                 state.items = state.items.filter(item => item.itemId !== id)
             } else {
-                
+
                 existingItem.quantity--
                 existingItem.totalPrice = existingItem.totalPrice - existingItem.price
             }
         }
     }
 })
+
+export const sendCartDate = (cart) => {
+    return async (dispatch) => {
+        dispatch(uiActions.showNotification({
+            status: 'pending',
+            title: 'Pending...',
+            message: 'Sending cart data!'
+        }))
+        const sendRequest = async () => {
+            const response = await fetch('https://reactdb-1e67f-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+                method: 'PUT',
+                body: JSON.stringify(cart)
+            })
+            if (!response.ok) {
+                throw new Error('Sending cart data failed')
+            }
+        }
+        try {
+            await sendRequest();
+            dispatch(uiActions.showNotification({
+                status: 'success',
+                title: 'Success',
+                message: 'Sending cart data succeeded!'
+            }))
+        } catch (error) {
+            dispatch(uiActions.showNotification({
+                status: 'error',
+                title: 'Error!',
+                message: 'Sending cart data failed!'
+            }))
+        }
+    }
+
+}
 export const cartActions = cartSlice.actions
 export default cartSlice

fb339bf87b950e2d9d1f4c42e5e6b6021bbabee9
ZyadYhiaJisr <z.yhia@jisr.net>
2025-02-05 16:30:17 +0200

    Add Multiple defer | Finalize the module

--- 22-1-adv-project/frontend/src/pages/EventDetails.js
+++ 22-1-adv-project/frontend/src/pages/EventDetails.js
@@ -1,15 +1,24 @@
 import { Suspense } from "react";
 import { useRouteLoaderData, redirect, Await } from "react-router-dom";
 import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
 
 function EventDetailsPage() {
-    const { event } = useRouteLoaderData('event-details');
+    const { event, events } = useRouteLoaderData('event-details');
     return (
-        <Suspense fallback={<p>Loading...</p>}>
-            <Await resolve={event}>
-                {data => <EventItem event={data} />}
-            </Await>
-        </Suspense>
+        <>
+            <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+                <Await resolve={event}>
+                    {loadedEvent => <EventItem event={loadedEvent} />}
+                </Await>
+            </Suspense>
+
+            <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+                <Await resolve={events}>
+                    {loadedEvents => <EventsList events={loadedEvents} />}
+                </Await>
+            </Suspense>
+        </>
     );
 }
 
@@ -26,10 +35,26 @@ const fetchEvent = async (params) => {
         return resData.event
     }
 }
+const fetchEvents = async () => {
+    const response = await fetch('http://localhost:8080/events');
 
-export const loader = ({request, params}) => {
+    if (!response.ok) {
+        throw new Response(JSON.stringify({ message: "Failed to fetch events" }), {
+            status: 500,
+        });
+    } else {
+        // return response;
+        const resData = await response.json();
+        return resData.events
+    }
+}
+
+export const loader = async ({ request, params }) => {
     return {
-        event: fetchEvent(params),
+        // await here will force react to not render the page until the promise is resolved
+        // and the another defer will be loaded after the page is rendered
+        event: await fetchEvent(params),
+        events: fetchEvents()
     }
 }
 export async function action({ request, params }) {

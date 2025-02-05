4b559eb70be42804b6f9a0f6d43b93dd362da4c7
ZyadYhiaJisr <z.yhia@jisr.net>
2025-02-05 14:41:59 +0200

    Adding UI submitting

--- 22-1-adv-project/frontend/src/components/EventForm.js
+++ 22-1-adv-project/frontend/src/components/EventForm.js
@@ -1,9 +1,11 @@
-import { Form, useNavigate } from 'react-router-dom';
+import { Form, useNavigate, useNavigation } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
   const navigate = useNavigate();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
   function cancelHandler() {
     navigate('..');
   }
@@ -14,7 +16,7 @@ function EventForm({ method, event }) {
     <Form method={method} className={classes.form}>
       <p>
         <label htmlFor="title">Title</label>
-        <input id="title" type="text" name="title" required defaultValue={event ? event?.title : '' } />
+        <input id="title" type="text" name="title" required defaultValue={event ? event?.title : ''} />
       </p>
       <p>
         <label htmlFor="image">Image</label>
@@ -32,7 +34,7 @@ function EventForm({ method, event }) {
         <button type="button" onClick={cancelHandler}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>{isSubmitting? 'Submitting...' : 'Save'}</button>
       </div>
     </Form>
   );
